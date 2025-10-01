c9d3062b7ae11a3bcbeeceb5cb40b9fc3a0b4279
Alikhan812107 <amd812107@gmail.com>
2025-10-02 04:03:32 +0500

    Metrics and adding CLI

--- src/main/java/Metrics.java
+++ src/main/java/Metrics.java
@@ -1,3 +1,13 @@
 public class Metrics {
+    public long comps, swaps, writes, allocs;
+    public int maxDepth, curDepth;
 
+    public void reset() { comps = swaps = writes = allocs = 0; maxDepth = curDepth = 0; }
+    public void incComp() { comps++; }
+    public void incSwap() { swaps++; }
+    public void incWrite(int c) { writes += c; }
+    public void addAlloc(long bytes) { allocs += bytes; }
+
+    public void enter() { if (++curDepth > maxDepth) maxDepth = curDepth; }
+    public void exit() { curDepth--; }
 }
---
+++ src/test/java/ClosestPPTTest.java
@@ -0,0 +1,2 @@
+public class ClosestPPTTest {
+}
